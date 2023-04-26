00485e7f383bcff5b07fc1888ffb0b03253e07ca
Deepak TS <tsdeepak589@gmail.com>
2023-04-26 09:20:22 +0530

    Database changed to atlass

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ handlebars.registerHelper('gt', function(a, b) {
 //mongoose connection
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/TestProject");
+mongoose.connect(process.env.MONGODB_CONNECTION);
 
 //url path
 app.use((req, res, next) => {
